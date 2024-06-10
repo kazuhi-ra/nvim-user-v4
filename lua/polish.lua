3b1a953c795130c517db5acf5a6541db10f739d3
kazuhi-ra <kazuhira4126@gmail.com>
2024-06-10 22:32:51 +0900

    patch

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,2 +1,23 @@
 -- -- NeoVimを起動したらNeo-treeを開く
 vim.cmd.Neotree("focus")
+
+-- patch
+local M = require('astrolsp')
+local original_progress = M.progress
+
+M.progress = function(data)
+  if not data then
+    vim.api.nvim_err_writeln("Error: data is nil in progress function")
+    return
+  end
+  if not data.params then
+    if data.result then
+      data.params = data.result
+    else
+      vim.api.nvim_err_writeln("Error: data.params is nil in progress function")
+      return
+    end
+  end
+
+  original_progress(data)
+end
