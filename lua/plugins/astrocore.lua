c0bd0ae5b535a1e0f684df7bc59173d3714400f2
kazuhi-ra <kazuhira4126@gmail.com>
2024-09-10 15:57:20 +0900

    keymapping

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -41,32 +39,134 @@ return {
     },
     -- Mappings can be configured through AstroCore as well.
     -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
-    mappings = {
-      -- first key is the mode
-      n = {
-        -- second key is the lefthand side of the map
+		mappings = {
+			n = {
+				["<leader>b"] = { name = "Buffers" },
 
-        -- navigate buffer tabs
-        ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
-        ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
+				-- https://twitter.com/thinca/status/1200791599510245376
+				["p"] = "]p",
+				["P"] = "]P",
 
-        -- mappings seen under group name "Buffer"
-        ["<Leader>bd"] = {
-          function()
-            require("astroui.status.heirline").buffer_picker(
-              function(bufnr) require("astrocore.buffer").close(bufnr) end
-            )
-          end,
-          desc = "Close buffer from tabline",
-        },
+				["|"] = {
+					function()
+						vim.cmd.vsplit()
+						require("smart-splits").move_cursor_left()
+						require("astrocore.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1))
+						require("smart-splits").move_cursor_right()
+					end,
+					desc = "Vertical Split",
+				},
 
-        -- tables with just a `desc` key will be registered with which-key if it's installed
-        -- this is useful for naming menus
-        -- ["<Leader>b"] = { desc = "Buffers" },
+				-- 12行だけスクロール
+				["<C-d>"] = "5<C-e>",
+				["<C-u>"] = "5<C-y>",
 
-        -- setting a mapping to false will disable it
-        -- ["<C-S>"] = false,
-      },
-    },
+				-- 矢印キーをよく使うやつに割り当て
+				["<Up>"] = {
+					function()
+						local astro = require("astrocore")
+						local worktree = astro.file_worktree()
+						local flags = worktree
+								and (" --work-tree=%s --git-dir=%s"):format(worktree.toplevel, worktree.gitdir)
+								or ""
+						astro.toggle_term_cmd({ cmd = "lazygit " .. flags, direction = "float" })
+					end,
+					desc = "ToggleTerm lazygit",
+				},
+
+				-- ref: https://github.com/nvim-telescope/telescope.nvim/issues/605
+				["<Down>"] = {
+					function()
+						local opts = {}
+						opts.previewer = require("telescope.previewers").new_termopen_previewer({
+							get_command = function(entry)
+								return {
+									"git",
+									"-c",
+									"core.pager=delta",
+									"-c",
+									"delta.side-by-side=true",
+									"show",
+									entry.value .. "^!",
+									"--",
+									entry.current_file,
+								}
+							end,
+						})
+						require("telescope.builtin").git_bcommits(opts)
+					end,
+					desc = "Git commits (current file)",
+				},
+				["<Left>"] = {
+					function()
+						require("telescope.builtin").find_files({ hidden = true })
+					end,
+				},
+				["<Right>"] = {
+					function()
+						require("telescope.builtin").live_grep({
+							additional_args = function()
+								return { "--hidden", "--no-ignore" }
+							end,
+						})
+					end,
+					desc = "Find words in all files",
+				},
+
+				["<S-Up>"] = {
+					function()
+						require("astrocore").toggle_term_cmd("lazydocker")
+					end,
+					desc = "ToggleTerm lazydocker",
+				},
+
+				["<S-Down>"] = {
+					function()
+						require("gitsigns").blame_line({ full = true })
+					end,
+					desc = "View full Git blame",
+				},
+
+				-- 行頭・行末移動
+				-- https://twitter.com/yuki_ycino/status/1336527468434317317
+				["<S-h>"] = {
+					"(getline('.')[0 : col('.') - 2] =~# '^\\s\\+$' ? '0' : '^')",
+					expr = true,
+					remap = false,
+					desc = "Next buffer",
+				},
+				["<S-l>"] = "$",
+
+				["<C-Tab>"] = {
+					function()
+						require("astrocore.buffer").nav(vim.v.count > 0 and vim.v.count or 1)
+					end,
+					desc = "Next buffer",
+				},
+				["<C-S-Tab>"] = {
+					function()
+						require("astrocore.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1))
+					end,
+					desc = "Previous buffer",
+				},
+				["<C-w>"] = {
+					function()
+						require("astrocore.buffer").close()
+					end,
+					desc = "Close buffer",
+				},
+			},
+			i = {
+				["<C-b>"] = "<Left>",
+				["<C-f>"] = "<Right>",
+				["<C-p>"] = "<Up>",
+				["<C-n>"] = "<Down>",
+			},
+			t = {
+				["<C-h>"] = "<C-h>",
+				["<C-c>"] = "<C-c>",
+				["<C-l>"] = "<C-l>",
+			},
+		},
   },
 }
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,6 +2,62 @@ return {
 	"nvim-neo-tree/neo-tree.nvim",
 	opts = function(_, config)
 		config.sources = { "filesystem", "git_status" }
+
+		config.filesystem.window = {
+			mappings = {
+				["l"] = "child_or_image_preview_or_open",
+			},
+		}
+		config.filesystem.commands = {
+			child_or_image_preview_or_open = function(state)
+				local node = state.tree:get_node()
+				local absolutePath = node.path
+				if node.type == "directory" or node:has_children() then
+					if not node:is_expanded() then -- if unexpanded, expand
+						state.commands.toggle_node(state)
+					else -- if expanded and has children, seleect the next child
+						require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
+					end
+				else -- if not a directory just open it
+					local function GetFileExtension(url)
+						return url:match("^.+(%..+)$")
+					end
+					local function IsImage(url)
+						local extension = GetFileExtension(url)
+
+						if extension == ".bmp" then
+							return true
+						elseif extension == ".jpg" or extension == ".jpeg" then
+							return true
+						elseif extension == ".png" then
+							return true
+						elseif extension == ".gif" then
+							return true
+						elseif extension == ".icon" then
+							return true
+						end
+
+						return false
+					end
+
+					if IsImage(absolutePath) then
+						-- identify
+						local identify_command = "identify " .. absolutePath
+						local identify_result = vim.fn.systemlist(identify_command)
+						local truncated_result = table.concat(identify_result, " "):gsub(" ", "\\n")
+
+						local command = "silent !wezterm cli split-pane --top --percent 98 -- zsh -c 'wezterm imgcat "
+							.. absolutePath
+							.. '; echo "\\nImage Info: '
+							.. truncated_result
+							.. "\"; read'"
+						vim.api.nvim_command(command)
+					else
+						state.commands.open(state)
+					end
+				end
+			end,
+		}
 		config.filesystem.filtered_items = {
 			visible = true,
 			hide_dotfiles = false,
