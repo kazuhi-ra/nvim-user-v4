81922a5905d19269a719bc2ced0d82443e5645e8
kazuhi-ra <kazuhira4126@gmail.com>
2024-09-21 12:54:33 +0900

    diffの文字を変更

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -5,43 +5,43 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocore",
-  ---@type AstroCoreOpts
-  opts = {
-    -- Configure core features of AstroNvim
-    features = {
-      large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
-      cmp = true, -- enable completion at start
-      diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
-      highlighturl = true, -- highlight URLs at start
-      notifications = true, -- enable notifications at start
-    },
-    -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
-    diagnostics = {
-      virtual_text = true,
-      underline = true,
-    },
-    -- vim options can be configured here
-    options = {
-      opt = { -- vim.opt.<key>
-        relativenumber = true, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
-        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = false, -- sets vim.opt.wrap
+	"AstroNvim/astrocore",
+	---@type AstroCoreOpts
+	opts = {
+		-- Configure core features of AstroNvim
+		features = {
+			large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
+			autopairs = true, -- enable autopairs at start
+			cmp = true, -- enable completion at start
+			diagnostics_mode = 3, -- diagnostic mode on start (0 = off, 1 = no signs/virtual text, 2 = no virtual text, 3 = on)
+			highlighturl = true, -- highlight URLs at start
+			notifications = true, -- enable notifications at start
+		},
+		-- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
+		diagnostics = {
+			virtual_text = true,
+			underline = true,
+		},
+		-- vim options can be configured here
+		options = {
+			opt = { -- vim.opt.<key>
+				relativenumber = true, -- sets vim.opt.relativenumber
+				number = true, -- sets vim.opt.number
+				spell = false, -- sets vim.opt.spell
+				signcolumn = "yes", -- sets vim.opt.signcolumn to yes
+				wrap = false, -- sets vim.opt.wrap
 				foldcolumn = "0",
 				laststatus = 0,
-				fillchars = { eob = " ", diff = "▓" }
-      },
-      g = { -- vim.g.<key>
-        -- configure global vim variables (vim.g)
-        -- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
-        -- This can be found in the `lua/lazy_setup.lua` file
-      },
-    },
-    -- Mappings can be configured through AstroCore as well.
-    -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
+				fillchars = { eob = " ", diff = "▓" },
+			},
+			g = { -- vim.g.<key>
+				-- configure global vim variables (vim.g)
+				-- NOTE: `mapleader` and `maplocalleader` must be set in the AstroNvim opts or before `lazy.setup`
+				-- This can be found in the `lua/lazy_setup.lua` file
+			},
+		},
+		-- Mappings can be configured through AstroCore as well.
+		-- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
 		mappings = {
 			n = {
 				["<leader>b"] = { name = "Buffers" },
@@ -71,7 +71,7 @@ return {
 						local worktree = astro.file_worktree()
 						local flags = worktree
 								and (" --work-tree=%s --git-dir=%s"):format(worktree.toplevel, worktree.gitdir)
-								or ""
+							or ""
 						astro.toggle_term_cmd({ cmd = "lazygit " .. flags, direction = "float" })
 					end,
 					desc = "ToggleTerm lazygit",
@@ -109,7 +109,7 @@ return {
 					function()
 						require("telescope.builtin").live_grep({
 							additional_args = function()
-								return { "--hidden", "--no-ignore" }
+								return { "--hidden" }
 							end,
 						})
 					end,
@@ -177,5 +177,5 @@ return {
 				["<C-l>"] = "<C-l>",
 			},
 		},
-  },
+	},
 }
